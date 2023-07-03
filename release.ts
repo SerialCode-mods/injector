e8549ae7cd136d6d9c3ee2175db8b2287985fff1
Aeryle <contact.aeryle@pm.me>
2023-07-03 22:47:51 +0200

    :bug: include bundled mod name in output zip

--- release.ts
+++ release.ts
@@ -1,23 +1,43 @@
-import AdmZip from 'adm-zip';
-import { resolve } from 'path';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import config from './config.json';
+import AdmZip from "adm-zip";
+import { resolve } from "path";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import config from "./config.json";
+
+const toUpperCase = (text: string) => text[0].toUpperCase() + text.slice(1);
 
 const args = yargs(hideBin(process.argv))
-  .command('bun release.ts', 'bundle the mod into a zip file', yargs => {
-      return yargs
-        .option('include', {
-            describe: 'include another mod',
-            type: 'string',
-        });
+  .command("bun release.ts", "bundle the mod into a zip file", (yargs) => {
+    return yargs.option("include", {
+      describe: "include another mod",
+      type: "string",
+    });
   })
-  .parseSync()
+  .parseSync();
+
+const isString = (text: unknown): text is string => typeof text === "string";
 
 const adm = new AdmZip();
-adm.addLocalFolder('./modules', 'python-packages');
-adm.addLocalFolder('./assets', `mods/${config.injected_name}`);
+adm.addLocalFolder(
+  "./modules",
+  resolve("python-packages", config.injected_name)
+);
+adm.addLocalFolder("./assets", `mods/${config.injected_name}`);
 
-if (args.include && typeof args.include === 'string') adm.addLocalFolder(resolve(process.cwd(), args.include, 'assets'), `mods/${args._[0]}`);
+const include = args.include;
+const name = args._[0];
+const isPartOfBundle = isString(include);
+const hasName = isString(name);
+const buildName = [
+  config.name,
+  "mod",
+  config.versions.mod,
+  "for",
+  config.versions.game,
+  hasName ? `with ${toUpperCase(name)} included` : "",
+];
 
-adm.writeZip(`./build/${config.name} Mod ${config.versions.mod} for ${config.versions.game}.zip`);
+if (isPartOfBundle)
+  adm.addLocalFolder(resolve(process.cwd(), include, "assets"), `mods/${name}`);
+
+adm.writeZip(resolve(process.cwd(), buildName.join("")));
