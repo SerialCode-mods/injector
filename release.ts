49145e54ae62912510e20203683c4421093ae99d
Aeryle <contact.aeryle@pm.me>
2023-07-03 23:22:21 +0200

    :rotating_light: reformat release.ts

--- release.ts
+++ release.ts
@@ -1,28 +1,28 @@
-import AdmZip from "adm-zip";
-import { resolve } from "path";
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
-import config from "./config.json";
+import AdmZip from 'adm-zip';
+import { resolve } from 'path';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import config from './config.json';
 
 const toUpperCase = (text: string) => text[0].toUpperCase() + text.slice(1);
 
 const args = yargs(hideBin(process.argv))
-  .command("bun release.ts", "bundle the mod into a zip file", (yargs) => {
-    return yargs.option("include", {
-      describe: "include another mod",
-      type: "string",
+  .command('bun release.ts', 'bundle the mod into a zip file', (yargs) => {
+    return yargs.option('include', {
+      describe: 'include another mod',
+      type: 'string',
     });
   })
   .parseSync();
 
-const isString = (text: unknown): text is string => typeof text === "string";
+const isString = (text: unknown): text is string => typeof text === 'string';
 
 const adm = new AdmZip();
 adm.addLocalFolder(
-  "./modules",
-  resolve("python-packages", config.injected_name)
+  './modules',
+  resolve('python-packages', config.injected_name)
 );
-adm.addLocalFolder("./assets", `mods/${config.injected_name}`);
+adm.addLocalFolder('./assets', `mods/${config.injected_name}`);
 
 const include = args.include;
 const name = args._[0];
@@ -30,14 +30,14 @@ const isPartOfBundle = isString(include);
 const hasName = isString(name);
 const buildName = [
   config.name,
-  "mod",
+  'mod',
   config.versions.mod,
-  "for",
+  'for',
   config.versions.game,
-  hasName ? `with ${toUpperCase(name)} included` : "",
+  hasName ? `with ${toUpperCase(name)} included` : '',
 ];
 
 if (isPartOfBundle)
-  adm.addLocalFolder(resolve(process.cwd(), include, "assets"), `mods/${name}`);
+  adm.addLocalFolder(resolve(process.cwd(), include, 'assets'), `mods/${name}`);
 
-adm.writeZip(resolve(process.cwd(), "build", `${buildName.join(" ")}.zip`));
+adm.writeZip(resolve(process.cwd(), 'build', `${buildName.join(' ')}.zip`));
